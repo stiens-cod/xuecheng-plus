0ff3350c95c301db5656f8124c91d77ee9b41595
shengdanxue <email>
2025-07-16 23:35:34 +0800

    2025年7月16日23:35:11 实现多种认证方式

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.xuecheng.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,14 +26,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     //配置用户信息服务
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService() {
         //这里配置用户信息,这里暂时使用这种方式将用户存储在内存中
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
         manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
         return manager;
-    }
+    }*/
 
 
     @Bean
@@ -57,6 +59,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout().logoutUrl("/logout");//退出地址
     }
 
+    @Autowired
+    DaoAuthenticationProviderCustom daoAuthenticationProviderCustom;
 
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProviderCustom);
+    }
 
 }
