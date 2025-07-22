df8bbc1df54efd0f87bbc37a257007e44e94180e
shengdanxue <email>
2025-07-22 18:57:34 +0800

    2025年7月22日18:57:20  添加选课整体完成

--- xuecheng-plus-learning/xuecheng-plus-learning-api/src/main/java/com/xuecheng/learning/api/MyCourseTablesController.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-api/src/main/java/com/xuecheng/learning/api/MyCourseTablesController.java
@@ -6,6 +6,7 @@ import com.xuecheng.learning.model.dto.MyCourseTableParams;
 import com.xuecheng.learning.model.dto.XcChooseCourseDto;
 import com.xuecheng.learning.model.dto.XcCourseTablesDto;
 import com.xuecheng.learning.model.po.XcCourseTables;
+import com.xuecheng.learning.service.MyCourseTablesService;
 import com.xuecheng.learning.util.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -29,18 +30,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyCourseTablesController {
 
 
+    @Autowired
+    MyCourseTablesService courseTablesService;
+
     @ApiOperation("添加选课")
     @PostMapping("/choosecourse/{courseId}")
     public XcChooseCourseDto addChooseCourse(@PathVariable("courseId") Long courseId) {
+        //登录用户
+        SecurityUtil.XcUser user = SecurityUtil.getUser();
+        if(user == null){
+            XueChengPlusException.cast("请登录后继续选课");
+        }
+        String userId = user.getId();
+        return  courseTablesService.addChooseCourse(userId, courseId);
 
-        return null;
     }
 
     @ApiOperation("查询学习资格")
     @PostMapping("/choosecourse/learnstatus/{courseId}")
     public XcCourseTablesDto getLearnstatus(@PathVariable("courseId") Long courseId) {
-
-        return null;
+        //登录用户
+        SecurityUtil.XcUser user = SecurityUtil.getUser();
+        if(user == null){
+            XueChengPlusException.cast("请登录后继续选课");
+        }
+        String userId = user.getId();
+        return  courseTablesService.getLearningStatus(userId, courseId);
 
     }
 
---
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/MyCourseTablesService.java
@@ -0,0 +1,38 @@
+package com.xuecheng.learning.service;
+
+import com.xuecheng.content.model.po.CoursePublish;
+import com.xuecheng.learning.model.dto.XcChooseCourseDto;
+import com.xuecheng.learning.model.dto.XcCourseTablesDto;
+import com.xuecheng.learning.model.po.XcChooseCourse;
+import com.xuecheng.learning.model.po.XcCourseTables;
+
+/**
+ * @description 我的课程表service接口
+ * @author Mr.M
+ * @date 2022/10/2 16:07
+ * @version 1.0
+ */
+public interface MyCourseTablesService {
+
+    /**
+     * @description 添加选课
+     * @param userId 用户id
+     * @param courseId 课程id
+     * @return com.xuecheng.learning.model.dto.XcChooseCourseDto
+     * @author Mr.M
+     * @date 2022/10/24 17:33
+     */
+    public XcChooseCourseDto addChooseCourse(String userId, Long courseId);
+
+    public XcChooseCourse addFreeCoruse(String userId, CoursePublish coursepublish);
+
+    public XcChooseCourse addChargeCoruse(String userId,CoursePublish coursepublish);
+
+    public XcCourseTables addCourseTabls(XcChooseCourse xcChooseCourse);
+
+    public XcCourseTables getXcCourseTables(String userId,Long courseId);
+
+    public XcCourseTablesDto getLearningStatus(String userId, Long courseId);
+
+
+}
