9ba6aca98371ebd28b631bc0ca8e6c6fc4d393b2
shengdanxue <email>
2025-07-23 22:21:06 +0800

    2025年7月23日22:20:51 我的课表

--- xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/MyCourseTablesService.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/MyCourseTablesService.java
@@ -1,6 +1,8 @@
 package com.xuecheng.learning.service;
 
+import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.po.CoursePublish;
+import com.xuecheng.learning.model.dto.MyCourseTableParams;
 import com.xuecheng.learning.model.dto.XcChooseCourseDto;
 import com.xuecheng.learning.model.dto.XcCourseTablesDto;
 import com.xuecheng.learning.model.po.XcChooseCourse;
@@ -35,6 +37,8 @@ public interface MyCourseTablesService {
     public XcCourseTablesDto getLearningStatus(String userId, Long courseId);
 
     public boolean saveChooseCourseSuccess(String chooseCourseId);
+
+    public PageResult<XcCourseTables> mycoursetables(MyCourseTableParams params);
 
 
 }
